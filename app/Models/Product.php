24c3a3df9dbe876bc774ecdc5454877ad9b1557f
vohuuphuoc <voroberrt@gmail.com>
2024-11-20 20:33:07 +0700

    gsgd

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'products';
+
+    protected $fillable = [
+        'name',
+        'amount',
+        'description',
+        'create',
+        'sold',
+        'price',
+        'size',
+        'rate',
+    ];
+
+    public $timestamps = true; 
 }
